7b31131193c97f1e8d886cd29d4e5c1e6c96361a
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-22 19:28:43 +0300

    feature: create homepage

--- app.vue
+++ app.vue
@@ -1,17 +1,7 @@
 <template>
-  <div class="dark">
-    <header class="antialiased text-slate-500 bg-white dark:bg-slate-900 w-full h-16">
-      <nav class="flex items-center justify-center w-full h-full text-center">
-        <h2 class="text-2xl font-bold">Hello world</h2>
-      </nav>
-      as
-    </header>
-  </div>
+  <Html lang="en" class="dark">
+    <NuxtPage />
+  </Html>
 </template>
 
-<script setup>
-onMounted(() => {
-  console.log('mounted');
-  console.log('fs');
-});
-</script>
+<script lang="ts" setup></script>
---
+++ pages/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+  <img src="https://store.adveits.com/project/oxer/react-js/demo/assets/img/demo/01_img.jpg" alt="" />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+img {
+  width: 100%;
+  height: auto;
+}
+</style>
