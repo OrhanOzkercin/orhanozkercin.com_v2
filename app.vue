64620b6eeb798b6eb3324422649582a99e7b530c
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-24 23:25:54 +0300

    fix: style of heigt

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
-  <Html lang="en" class="dark">
-    <Body class="container mx-auto">
+  <Html lang="en" class="dark h-full">
+    <Body class="container mx-auto min-h-screen">
       <NuxtPage />
     </Body>
   </Html>
