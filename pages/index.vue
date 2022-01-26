847c1d99f00aaf1282ef2b2a21a5fc06462779d3
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-27 00:12:38 +0300

    style: add rubberBand animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,50 @@
 <template>
   <Header />
   <main class="relative">
-    <div class="absolute top-16 left-0">
+    <div ref="headerWord" class="absolute top-16 left-0">
       <span
-        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk hover:animate-rubber-band text-transparent bg-clip-text bg-gradient-to-t from-slate-900 to-slate-400 dark:from-slate-100 dark:to-slate-700"
-        >H</span
-      >
-      <span
-        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk hover:animate-rubber-band text-transparent bg-clip-text bg-gradient-to-tr from-slate-900 to-slate-400 dark:from-slate-100 dark:to-slate-700"
-        >e</span
-      >
-      <span
-        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk hover:animate-rubber-band text-transparent bg-clip-text bg-gradient-to-r from-slate-900 to-slate-400 dark:from-slate-100 dark:to-slate-700"
-        >l</span
-      >
-      <span
-        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk hover:animate-rubber-band text-transparent bg-clip-text bg-gradient-to-br from-slate-900 to-slate-400 dark:from-slate-100 dark:to-slate-700"
-        >l</span
-      >
-      <span
-        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk hover:animate-rubber-band text-transparent bg-clip-text bg-gradient-to-b from-slate-900 to-slate-400 dark:from-slate-100 dark:to-slate-700"
-        >o,</span
-      >
+        class="inline-block text-4xl md:text-6xl lg:text-9xl tracking-wide font-pofuduk cursor-default text-transparent bg-clip-text bg-gradient-to-t from-slate-900 to-slate-400 dark:from-slate-50 dark:to-slate-100"
+        v-for="(letter, index) in helloWord"
+        >{{ letter }}
+      </span>
     </div>
   </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const helloWord = 'Hello';
+const headerWord = ref<HTMLElement>(null);
+
+onMounted(() => {
+  listenHoverOnLetters(headerWord.value);
+});
+
+function listenHoverOnLetters(headerWord: HTMLElement) {
+  headerWord.addEventListener('mouseover', (e) => {
+    const element = e.target as HTMLElement;
+    if (element.tagName == 'DIV') return;
+
+    console.log('element:', element);
+    element.classList.add('animated');
+    element.classList.add('rubberBand');
+
+    element.classList.remove('bg-gradient-to-t');
+    element.classList.remove('from-slate-900');
+    element.classList.remove('to-slate-400');
+    element.classList.add('dark:bg-primary');
+  });
+  headerWord.addEventListener('animationend', (e) => {
+    const element = e.target as HTMLElement;
+    element.classList.remove('animated');
+    element.classList.remove('rubberBand');
+
+    element.classList.add('bg-gradient-to-t');
+    element.classList.add('from-slate-900');
+    element.classList.add('to-slate-400');
+    element.classList.remove('dark:bg-primary');
+  });
+}
+</script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@700&display=swap');
