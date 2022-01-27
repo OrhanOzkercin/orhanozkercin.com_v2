ff1cab5c1682000353a480ff1b6a22d5a71ead24
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-28 00:14:34 +0300

    feature: create ThrPrimaryButton compoent

---
+++ components/ThePrimaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+  <button class="border-2 border-primary px-16 py-3 text-primary font-medium tracking-widest"><slot /></button>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped></style>
