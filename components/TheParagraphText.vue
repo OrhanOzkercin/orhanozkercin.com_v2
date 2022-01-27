e648307fb3136a60c059f1badae0d3c50661c1be
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-01-28 00:13:53 +0300

    feature: create theParagraphText.vue compoent

---
+++ components/TheParagraphText.vue
@@ -0,0 +1,33 @@
+<template>
+  <p class="paragraph">
+    <slot />
+  </p>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.paragraph {
+  font-size: 2rem;
+}
+.paragraph::before,
+.paragraph::after {
+  font-family: 'Comforter', cursive;
+}
+.paragraph::before {
+  content: '<p>';
+  color: #515152;
+  font-size: 1.5rem;
+  position: absolute;
+  top: -2.5rem;
+  // margin-left: -1.5rem;
+}
+.paragraph::after {
+  content: '</p>';
+  color: #515152;
+  font-size: 1.5rem;
+  position: absolute;
+  bottom: -2.5rem;
+  left: 0;
+}
+</style>
