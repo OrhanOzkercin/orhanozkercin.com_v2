deda8bc66998980c2b60f94dab616a84f88c7a11
Orhan Özkerçin <orhanozkercin@yahoo.com>
2022-02-08 02:12:29 +0300

    style: update color and  split btn style

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,10 +22,13 @@ module.exports = {
     extend: {
       colors: {
         primary: colors.yellow[500],
+        secondary: colors.cyan[500],
         "text-color": colors.slate[400],
+        "after-color": colors.gray[200]
       },
       boxShadow: ({ theme }) => ({
         "primary-button": `inset 0 0 0 2px ${theme("colors.primary")}`,
+        "secondary-button": `inset 0 0 0 2px ${theme("colors.secondary")}`,
       }),
 
       fontFamily: {
